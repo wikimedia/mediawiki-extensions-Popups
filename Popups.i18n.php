f5a64822524f47be7f1f9d8291aac20a42769aa3
Yuvi Panda <yuvipanda@gmail.com>
2014-02-06 15:51:39 +0530

    Base PHP Extension description files
    Change-Id: Ic210ab97494071672eceee558600002b04e668df

---
+++ Popups.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * This file is part of the MediaWiki extension Popups..
+ *
+ * Popups is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Popups is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Popups.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file
+ * @ingroup extensions
+ */
+
+$messages = array();
+
+/**
+ * English
+ */
+$messages['en'] = array(
+	'popups-desc' => 'Displays popups when you hover over article links',
+);
+
+/** Message documentation (Message documentation)
+ */
+$messages['qqq'] = array(
+	'popups-desc' => '{{desc|name=Popups|url=https://www.mediawiki.org/wiki/Extension:Popups}}',
+);
