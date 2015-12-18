1dc7725389e873aec5a948da511440ac16ef2a35
Kunal Mehta <legoktm@member.fsf.org>
2015-12-18 14:05:11 -0800

    Use ExtensionRegistry to determine whether TextExtracts is installed
    Bug: T121777
    Change-Id: Ic017615993b7d1ba746424ce6b576b71ba72b25f

--- Popups.hooks.php
+++ Popups.hooks.php
@@ -126,7 +126,8 @@ class PopupsHooks {
 		// Enable only if the user has turned it on in Beta Preferences, or BetaFeatures is not installed.
 		// Will only be loaded if PageImages & TextExtracts extensions are installed.
 
-		if ( !defined( 'TEXT_EXTRACTS_INSTALLED' ) || !class_exists( 'ApiQueryPageImages' ) ) {
+		$registry = ExtensionRegistry::getInstance();
+		if ( !$registry->isLoaded( 'TextExtracts' ) || !class_exists( 'ApiQueryPageImages' ) ) {
 			$logger = LoggerFactory::getInstance( 'popups' );
 			$logger->error( 'Popups requires the PageImages and TextExtracts extensions.' );
 			return true;
