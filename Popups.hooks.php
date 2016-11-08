f64c6ea50988e982a390b12273ef38b15f86bfba
Sam Smith <git@samsmith.io>
2016-11-08 08:03:20 +0000

    Disable QUnit and acceptance tests
    Change-Id: Icee115520ec559f8e107de2946224ac1fe65154d

--- Popups.hooks.php
+++ Popups.hooks.php
@@ -227,23 +227,12 @@ class PopupsHooks {
 	 * @return bool
 	 */
 	public static function onResourceLoaderTestModules( array &$testModules, ResourceLoader &$resourceLoader ) {
-		$testModules['qunit']['ext.popups.tests'] = array(
-			'scripts' => array(
-				'tests/qunit/ext.popups.desktopRenderer.test.js',
-				'tests/qunit/ext.popups.renderer.article.test.js',
-				'tests/qunit/ext.popups.core.test.js',
-				'tests/qunit/ext.popups.schemaPopups.utils.test.js',
-				'tests/qunit/ext.popups.settings.test.js',
-				'tests/qunit/ext.popups.experiment.test.js',
-			),
-			'dependencies' => array(
-				'ext.popups.desktop',
-				'ext.popups.schemaPopups.utils'
-			),
-			'localBasePath' => __DIR__,
-			'remoteExtPath' => 'Popups',
-		);
-		return true;
+		//$testModules['qunit']['ext.popups.tests'] = [
+		//	'scripts' => [],
+		//	'dependencies' => [],
+		//	'localBasePath' => __DIR__,
+		//	'remoteExtPath' => 'Popups',
+		//];
 	}
 
 	/**
