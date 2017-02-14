0260325bb995fd18b5680caba8127d3f66a94f99
joakin <joaquin@chimeces.com>
2017-02-14 21:17:48 +0100

    Hygiene: Remove unnecessary IIFE in schema.js
    Change-Id: Ic152668ff7e4c96fb63934283897a51e81e58dd4

--- resources/dist/index.js
+++ resources/dist/index.js
@@ -2126,31 +2126,30 @@
 /* 19 */
 /***/ function(module, exports) {
 
-	( function ( mw, $ ) {
+	var mw = window.mediaWiki,
+		$ = jQuery;
 	
-		/**
-		 * Creates an instance of an EventLogging schema that can be used to log
-		 * Popups events.
-		 *
-		 * @param {mw.Map} config
-		 * @param {Window} window
-		 * @return {mw.eventLog.Schema}
-		 */
-		module.exports = function ( config, window ) {
-			var samplingRate = config.get( 'wgPopupsSchemaSamplingRate', 0 );
+	/**
+	 * Creates an instance of an EventLogging schema that can be used to log
+	 * Popups events.
+	 *
+	 * @param {mw.Map} config
+	 * @param {Window} window
+	 * @return {mw.eventLog.Schema}
+	 */
+	module.exports = function ( config, window ) {
+		var samplingRate = config.get( 'wgPopupsSchemaSamplingRate', 0 );
 	
-			if (
-				!window.navigator ||
-				!$.isFunction( window.navigator.sendBeacon ) ||
-				window.QUnit
-			) {
-				samplingRate = 0;
-			}
+		if (
+			!window.navigator ||
+			!$.isFunction( window.navigator.sendBeacon ) ||
+			window.QUnit
+		) {
+			samplingRate = 0;
+		}
 	
-			return new mw.eventLog.Schema( 'Popups', samplingRate );
-		};
-	
-	}( mediaWiki, jQuery ) );
+		return new mw.eventLog.Schema( 'Popups', samplingRate );
+	};
 
 
 /***/ },
