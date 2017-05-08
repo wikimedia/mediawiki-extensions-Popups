7bd29bb0582fc3d592ce2c242ac12df205d3a537
joakin <joaquin@chimeces.com>
2017-05-08 20:50:59 +0200

    Enable production settings for the production bundle
    It enables certain optimizations, see:
    * https://webpack.js.org/guides/production-build/
    
    Code is minified by uglifyjs.
    
    Aditional changes:
    * Add banner so that sources are not minified by ResourceLoader
    
    Bug: T160061
    Change-Id: I50c9148ebf2d860db42a24225bc128bfcfe56927

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
-/* global __dirname */
+/* global __dirname, process */
 
 var path = require( 'path' ),
 	webpack = require( 'webpack' ),
-	PUBLIC_PATH = '/w/extensions/Popups';
+	PUBLIC_PATH = '/w/extensions/Popups',
+	isProduction = process.env.NODE_ENV === 'production',
+	reduxPath,
+	reduxThunkPath,
+	conf;
 
-module.exports = {
+reduxPath = isProduction ?
+	'node_modules/redux/dist/redux.min.js' :
+	'node_modules/redux/dist/redux.js';
+
+reduxThunkPath = isProduction ?
+	'node_modules/redux-thunk/dist/redux-thunk.min.js' :
+	'node_modules/redux-thunk/dist/redux-thunk.js';
+
+conf = {
 	output: {
 		// The absolute path to the output directory.
 		path: path.resolve( __dirname, 'resources/dist' ),
@@ -20,14 +32,48 @@ module.exports = {
 	devtool: 'source-map',
 	resolve: {
 		alias: {
-			redux: path.resolve( __dirname, 'node_modules/redux/dist/redux.js' ),
-			'redux-thunk': path.resolve( __dirname, 'node_modules/redux-thunk/dist/redux-thunk.js' )
+			redux: path.resolve( __dirname, reduxPath ),
+			'redux-thunk': path.resolve( __dirname, reduxThunkPath )
 		}
 	},
 	plugins: [
 		// To generate identifiers that are preserved over builds, webpack supplies
 		// the NamedModulesPlugin (generates comments with file names on bundle)
 		// https://webpack.js.org/guides/caching/#deterministic-hashes
-		new webpack.NamedModulesPlugin()
+		new webpack.NamedModulesPlugin(),
+
+		// Disable ResourceLoader minification since we're going to be minifying
+		// with uglify
+		new webpack.BannerPlugin( {
+			banner: '/*@nomin*/', // ResourceLoader::FILTER_NOMIN
+			// Don't wrap the banner in a comment. It's easier to keep the banner in
+			// sync with ResourceLoader::FILTER_NOMIN this way
+			raw: true
+		} )
 	]
 };
+
+// Production settings.
+// Enable minification and dead code elimination with uglify. Define the
+// global process.env.NODE_ENV so that libraries like redux and redux-thunk get
+// development code trimmed.
+// Enable minimize flags for webpack loaders and disable debug.
+if ( isProduction ) {
+	conf.plugins = conf.plugins.concat( [
+		new webpack.LoaderOptionsPlugin( {
+			minimize: true,
+			debug: false
+		} ),
+		new webpack.DefinePlugin( {
+			'process.env': {
+				NODE_ENV: JSON.stringify( 'production' )
+			}
+		} ),
+		new webpack.optimize.UglifyJsPlugin( {
+			sourceMap: true,
+			comments: /@nomin/
+		} )
+	] );
+}
+
+module.exports = conf;
