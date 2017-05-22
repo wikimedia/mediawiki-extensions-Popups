9350df7111098482ecd5f486aecb7c6e34ba950d
Sam Smith <me@samsmith.io>
2017-05-22 19:16:17 +0100

    doc: Document schema module
    Bug: T158236
    Change-Id: I365fbfc1725d92003e902301252d53d4620c2128

--- src/schema.js
+++ src/schema.js
@@ -1,9 +1,17 @@
+/**
+ * @module schema
+ */
+
 var mw = window.mediaWiki,
 	$ = jQuery;
 
 /**
- * Creates an instance of an EventLogging schema that can be used to log
- * Popups events.
+ * Creates an instance of the [EventLogging Schema class][0] with a sampling
+ * rate of `wgPopupsSchemaSamplingRate` if the UA supports [the Beacon API][1]
+ * or `0` if it doesn't.
+ *
+ * [0]: https://github.com/wikimedia/mediawiki-extensions-EventLogging/blob/master/modules/ext.eventLogging.Schema.js
+ * [1]: https://w3c.github.io/beacon/
  *
  * @param {mw.Map} config
  * @param {Window} window
