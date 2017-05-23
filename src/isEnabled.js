e9fb33af3ab709e5452a064028e454eac0a897ff
Sam Smith <me@samsmith.io>
2017-05-23 12:01:04 +0100

    doc: Document isEnabled module
    Bug: T158236
    Change-Id: Ib80a5951e41765b982fecf4e2ac33767e4ef1738

--- src/isEnabled.js
+++ src/isEnabled.js
@@ -1,19 +1,23 @@
 /**
+ * @module isEnabled
+ */
+
+/**
  * Given the global state of the application, creates a function that gets
  * whether or not the user should have Page Previews enabled.
  *
- * Page Preview is disabled when the Navigation Popups gadget is enabled.
+ * Page Previews is disabled when the Navigation Popups gadget is enabled.
  *
  * If Page Previews is configured as a beta feature (see
  * `$wgPopupsBetaFeature`), the user must be logged in and have enabled the
  * beta feature in order to see previews. Logged out users won't be able
  * to see the feature.
  *
- * If Page Previews is configured as a preference, then the user must either
- * be logged in and have enabled the preference or be logged out and have not
- * disabled previews via the settings modal. Logged out users who have not
- * disabled or enabled the previews via the settings modal are sampled at
- * the sampling rate defined by `wgPopupsAnonsEnabledSamplingRate`.
+ * If Page Previews is configured as a user preference, then the user must
+ * either be logged in and have enabled the preference or be logged out and have
+ * not disabled previews via the settings modal. Logged out users who have not
+ * disabled or enabled the previews via the settings modal are sampled at the
+ * sampling rate defined by `wgPopupsAnonsEnabledSamplingRate`.
  *
  * @param {mw.user} user The `mw.user` singleton instance
  * @param {Object} userSettings An object returned by `userSettings.js`
