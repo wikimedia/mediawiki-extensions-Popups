cb72bf4e823d4432b8949f7a1281663e10cbec7b
Piotr Miazga <piotr@polishdeveloper.pl>
2017-08-29 16:37:46 +0200

    Do not use keyword `const` as it's part of ES6 syntax
    Changes:
      - instead of const use var
    
    Bug: T174424
    Change-Id: I3d786614b5acbfe9a05c332edd3a14c2e5afa417

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,14 @@
 var $ = jQuery,
 	// If bracketedDevicePixelRatio is not available default to 1 (in tests for
 	// example)
-	pixelRatio = $.bracketedDevicePixelRatio && $.bracketedDevicePixelRatio() || 1;
+	pixelRatio = $.bracketedDevicePixelRatio && $.bracketedDevicePixelRatio() || 1,
+	BUCKETS = {
+		off: 'off',
+		on: 'on',
+		control: 'control'
+	};
 
-export const BUCKETS = {
-	off: 'off',
-	on: 'on',
-	control: 'control'
-};
+export { BUCKETS };
 
 export default {
 	THUMBNAIL_SIZE: 300 * pixelRatio,
