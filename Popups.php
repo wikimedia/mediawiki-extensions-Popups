27cfa1ce02d07d636c0d74b6182c26a90a723ae4
Fomafix <fomafix@googlemail.com>
2018-08-24 18:32:21 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I01f3fd805b76c666eedc358d3c860248011622e4

--- Popups.php
+++ Popups.php
@@ -26,10 +26,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Popups' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Popups'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Popups extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the Popups extension requires MediaWiki 1.30+' );
