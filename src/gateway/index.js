0b64e803f23b128e04d98a2fa304b6ce513fcaf1
WMDE-Fisch <christoph.jauera@wikimedia.de>
2019-02-07 16:25:21 +0100

    Move gateway typedefs to gateway index
    When we started reorganising the code around the gateway selection
    we forgot moving the docs and typedefs from the (now) more specific
    page.js file to the (now new) index.js. - This patch fixes that.
    
    See also I5efa9fb8f63f1487c627eb9a3f1fe47f43c611cc
    
    Change-Id: I7b3582882247e22497d3a37b896a8a7c9950cf0d

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -7,6 +7,39 @@ import { previewTypes } from '../preview/model';
 const $ = jQuery;
 
 /**
+ * The interface implemented by all preview gateways.
+ * @typedef Gateway
+ * @prop {Function(string): JQuery.jqXHR} fetch
+ * @prop {FetchPreviewForTitle} fetchPreviewForTitle
+ * @prop {ConvertPageToModel} convertPageToModel
+ */
+
+/**
+ * A Promise, usually for a long running or costly task such as an HTTP request,
+ * that is abortable.
+ * @template T
+ * @typedef {JQuery.Promise<T>} AbortPromise
+ * @prop {Function(): void} abort
+ */
+
+/**
+ * Fetches a preview for a page or reference.
+ *
+ * If the underlying request is successful and contains data for the requested title,
+ * then the resulting promise will resolve. If not, then it will reject.
+ *
+ * @typedef {Function(mw.Title): AbortPromise<PreviewModel>} FetchPreviewForTitle
+ */
+
+/**
+ * Converts the API response to a preview model. Exposed for testing only.
+ *
+ * @typedef {Function(object, ...any): PreviewModel} ConvertPageToModel
+ */
+
+/**
+ * Select the general gateway type depending on the applicable popup type.
+ *
  * @param {Element} el
  * @param {mw.Map} config
  * @param {mw.Title} title
--- src/gateway/page.js
+++ src/gateway/page.js
@@ -11,38 +11,7 @@ const mw = mediaWiki,
 	$ = jQuery;
 
 /**
- * The interface implemented by all preview gateways.
- * @typedef Gateway
- * @prop {Function(string): JQuery.jqXHR} fetch
- * @prop {FetchPreviewForTitle} fetchPreviewForTitle
- * @prop {ConvertPageToModel} convertPageToModel
- */
-
-/**
- * A Promise, usually for a long running or costly task such as an HTTP request,
- * that is abortable.
- * @template T
- * @typedef {JQuery.Promise<T>} AbortPromise
- * @prop {Function(): void} abort
- */
-
-/**
- * Fetches a preview for a page or reference.
- *
- * If the underlying request is successful and contains data for the requested title,
- * then the resulting promise will resolve. If not, then it will reject.
- *
- * @typedef {Function(mw.Title): AbortPromise<PreviewModel>} FetchPreviewForTitle
- */
-
-/**
- * Converts the API response to a preview model. Exposed for testing only.
- *
- * @typedef {Function(object, ...any): PreviewModel} ConvertPageToModel
- */
-
-/**
- * Creates a gateway with sensible values for the dependencies.
+ * Creates a page preview gateway with sensible values for the dependencies.
  *
  * @param {mw.Map} config
  * @return {Gateway}
