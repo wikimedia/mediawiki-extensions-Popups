8b6e85dd7c028b1dbcc45a5e3be65f33c1f7155e
Volker E <volker.e@wikimedia.org>
2020-09-18 13:16:18 -0700

    storybook: Update to account for new core LESS variables files
    We're using Vector's 'mediawiki.skin.variables.less'
    file, as it's Popups main interface.
    
    Bug: T263064
    Change-Id: Ib2b3726dddb95e8f7ae54e6c2d4cf52c70d0197a

--- .storybook/storybook-resources.sh
+++ .storybook/storybook-resources.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
+# MediaWiki skin LESS variables, defaults in core, we're using Vector's skin.variables.
+curl -sSL "https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/core/+/master/resources/src/mediawiki.less/mediawiki.skin.defaults.less?format=TEXT" | base64 --decode > .storybook/mocks/mediawiki.skin.defaults.less
+curl -sSL "https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/skins/Vector/+/master/resources/mediawiki.less/mediawiki.skin.variables.less?format=TEXT" | base64 --decode > .storybook/mocks/mediawiki.skin.variables.less
+
+# MediaWiki LESS mixins
 curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.less/mediawiki.mixins.less -o .storybook/mocks/mediawiki.mixins.less
 curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.less/mediawiki.mixins.animation.less -o .storybook/mocks/mediawiki.mixins.animation.less
+
+# mediawiki.ui variables
 curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.less/mediawiki.ui/variables.less -o .storybook/mocks/mediawiki.ui/variables.less
+
+# mediawiki.ui icons.less
 curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.ui/components/icons.less -o .storybook/mocks/mediawiki.ui/components/icons.less
