6bc2077ed5637f1ae73a752d7c452ab8952e3132
Noam Rosenthal <noam.j.rosenthal@gmail.com>
2020-11-29 15:57:45 +0200

    Remove title attributes at init
    This is a performance optimization - removing
    all the titles when initializing the popup extension
    reduces DOM manipulation during hover, removing/reinstating the title
    attribute.
    
    When the popup extension is loaded, the default "title" behvior is unnecessary.
    
    Change-Id: I1a85394b6b67eabee50a8d554bfd9b62de2a24d3

--- src/changeListeners/index.js
+++ src/changeListeners/index.js
@@ -1,6 +1,5 @@
 import footerLink from './footerLink';
 import eventLogging from './eventLogging';
-import linkTitle from './linkTitle';
 import pageviews from './pageviews';
 import render from './render';
 import settings from './settings';
@@ -10,7 +9,6 @@ import syncUserSettings from './syncUserSettings';
 export default {
 	footerLink,
 	eventLogging,
-	linkTitle,
 	pageviews,
 	render,
 	settings,
