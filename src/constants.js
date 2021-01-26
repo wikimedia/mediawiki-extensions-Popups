83c528c7e1ba92492aa2a0f85e07d8e93c23ae8c
Noam Rosenthal <noam.j.rosenthal@gmail.com>
2021-01-26 08:10:39 +0200

    Fix bluriness on low-DPR screens
    Request a larger thumbnail to ensure
    the thumbnail is served scaled rather
    than scaled by the browser.
    
    Bug: T272169
    Change-Id: Ibf80f24c949e14c8289b8b0a4e7369dd10ead449

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ const bpr = bracketedPixelRatio();
 
 export default {
 	BRACKETED_DEVICE_PIXEL_RATIO: bpr,
-	THUMBNAIL_SIZE: 320 * bpr,
+	// See https://phabricator.wikimedia.org/T272169: requesting a larger thumbnail to avoid bluriness
+	THUMBNAIL_SIZE: 320 * Math.max( bpr, 1.5 ),
 	EXTRACT_LENGTH: 525
 };
