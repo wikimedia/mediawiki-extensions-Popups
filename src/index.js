a438b641c81fa16faba287407012beaff8b1f3ba
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2021-04-28 15:32:46 +0200

    Fix settings dialog offering ReferencePreviews when unavailable
    Open questions:
    * It is possible to make the patch smaller in case this makes
      it easier to backport it. Is this worth it?
    * This code is not covered by any test. I think it's worth
      writing tests. Most of this code will stay, even after beta.
    
    Bug: T281352
    Change-Id: I5f30054f1664643b427909f7fa189b4ea5e11879
    (cherry picked from commit 75155a076a0629cadb59f85a38a29bee16114c5a)

--- src/index.js
+++ src/index.js
@@ -170,7 +170,8 @@ function registerChangeListeners(
 		pagePreviewGateway = createPagePreviewGateway( mw.config ),
 		referenceGateway = createReferenceGateway(),
 		userSettings = createUserSettings( mw.storage ),
-		settingsDialog = createSettingsDialogRenderer( mw.config ),
+		referencePreviewsState = isReferencePreviewsEnabled( mw.user, userSettings, mw.config ),
+		settingsDialog = createSettingsDialogRenderer( referencePreviewsState !== null ),
 		experiments = createExperiments( mw.experiments ),
 		statsvTracker = getStatsvTracker( mw.user, mw.config, experiments ),
 		// Virtual pageviews are always tracked.
@@ -187,8 +188,7 @@ function registerChangeListeners(
 		initiallyEnabled = {
 			[ previewTypes.TYPE_PAGE ]:
 				createIsPagePreviewsEnabled( mw.user, userSettings, mw.config ),
-			[ previewTypes.TYPE_REFERENCE ]:
-				isReferencePreviewsEnabled( mw.user, userSettings, mw.config )
+			[ previewTypes.TYPE_REFERENCE ]: referencePreviewsState
 		};
 
 	// If debug mode is enabled, then enable Redux DevTools.
