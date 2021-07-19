1a62ae90cde931763c0fbbf06d1ecff98061ef9d
jdlrobson <jdlrobson@gmail.com>
2021-07-19 14:57:41 -0700

    Remove the page preview icon hacks
    Now the Vector icon stylesheet is aligned with the correct
    design specification these should no longer be needed.
    
    Additional changes:
    - Fixes icon display in storybook for disambiguation links
    
    Bug: T287058
    Depends-On: Ibc136a17662ae839f90babb21e0f7e8f27b7a7d5
    Change-Id: I02bfd7973352a03b5be4b6e82575e2209b8e4043

--- .storybook/storybook-resources.sh
+++ .storybook/storybook-resources.sh
@@ -13,7 +13,7 @@ curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/
 curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.less/mediawiki.ui/variables.less -o .storybook/mocks/mediawiki.ui/variables.less
 
 # mediawiki.ui icons.less
-curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.ui/components/icons.less -o .storybook/mocks/mediawiki.ui/components/icons.less
+curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.ui/components/icons-2.less -o .storybook/mocks/mediawiki.ui/components/icons.less
 
 # OOUI/WikimediaUI theme icons
-curl "https://en.wikipedia.org/w/load.php?modules=ext.popups.icons&only=styles&debug=true"  -o .storybook/mocks/production-icons.less
+curl "https://en.wikipedia.org/w/load.php?modules=ext.popups.icons|ext.popups.images&only=styles&debug=true"  -o .storybook/mocks/production-icons.less
