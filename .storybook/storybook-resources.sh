24925ec459b9621227bee934eee4fc525cb27397
Jon Robson <jdlrobson@gmail.com>
2023-03-22 11:32:25 -0700

    Fix storybook integration
    Broken by Ie456dfbd703a0e7a0ad783909ca3ef5e86450d9c
    
    Change-Id: I4b6a4817764210cbc4f9ce37be0f117dbc3b656b

--- .storybook/storybook-resources.sh
+++ .storybook/storybook-resources.sh
@@ -3,7 +3,8 @@ set -ex
 
 # MediaWiki skin LESS variables, defaults in core, we're using Vector's skin.variables.
 curl -sSL "https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/core/+/master/resources/src/mediawiki.less/mediawiki.skin.defaults.less?format=TEXT" | base64 --decode > .storybook/mocks/mediawiki.skin.defaults.less
-curl -sSL "https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/skins/Vector/+/master/resources/mediawiki.less/mediawiki.skin.variables.less?format=TEXT" | base64 --decode > .storybook/mocks/mediawiki.skin.variables.less
+curl -sSL "https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/skins/Vector/+/master/resources/mediawiki.less/vector-2022/mediawiki.skin.variables.less?format=TEXT" | base64 --decode > .storybook/mocks/mediawiki.skin.variables.less
+curl -sSL "https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/core/+/master/resources/lib/codex-design-tokens/theme-wikimedia-ui-legacy.less?format=TEXT" | base64 --decode > .storybook/mocks/mediawiki.skin.codex-design-tokens/theme-wikimedia-ui-legacy.less
 
 # MediaWiki LESS mixins
 curl https://raw.githubusercontent.com/wikimedia/mediawiki/master/resources/src/mediawiki.less/mediawiki.mixins.less -o .storybook/mocks/mediawiki.mixins.less
