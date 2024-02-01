f5dd9a0bbfb49f1131bdd7414c33967a0afc232c
Umherirrender <umherirrender_de.wp@web.de>
2024-02-01 19:17:39 +0100

    Add return type hint to functions in ServiceWiring.php
    This is a common pattern for service wiring functions
    
    Change-Id: I2c6a6b95ad5f9d69f90631ede39882e41e7c85df

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -1,28 +1,30 @@
 <?php
 
+use MediaWiki\Config\Config;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use Popups\PopupsContext;
 use Popups\PopupsGadgetsIntegration;
+use Psr\Log\LoggerInterface;
 
 /**
  * @codeCoverageIgnore
  */
 return [
-	'Popups.Config' => static function ( MediaWikiServices $services ) {
+	'Popups.Config' => static function ( MediaWikiServices $services ): Config {
 		return $services->getService( 'ConfigFactory' )
 			->makeConfig( PopupsContext::EXTENSION_NAME );
 	},
-	'Popups.GadgetsIntegration' => static function ( MediaWikiServices $services ) {
+	'Popups.GadgetsIntegration' => static function ( MediaWikiServices $services ): PopupsGadgetsIntegration {
 		return new PopupsGadgetsIntegration(
 			$services->getService( 'Popups.Config' ),
 			ExtensionRegistry::getInstance()
 		);
 	},
-	'Popups.Logger' => static function ( MediaWikiServices $services ) {
+	'Popups.Logger' => static function ( MediaWikiServices $services ): LoggerInterface {
 		return LoggerFactory::getInstance( PopupsContext::LOGGER_CHANNEL );
 	},
-	'Popups.Context' => static function ( MediaWikiServices $services ) {
+	'Popups.Context' => static function ( MediaWikiServices $services ): PopupsContext {
 		return new PopupsContext(
 			$services->getService( 'Popups.Config' ),
 			ExtensionRegistry::getInstance(),
