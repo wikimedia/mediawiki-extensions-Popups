eb1ddbb1321d12fc71412f44e50f9ca766ec24b7
Željko Filipin <zeljko.filipin@gmail.com>
2024-11-21 14:41:05 +0100

    selenium: Replace Node.js Assert library with WebdriverIO Expect library
    Assertions from Expect library are more readable that assertions from Assert library.
    
    Bug: T325740
    Change-Id: I9224c52975919a512ac3b0342bb3805edecec6b3

--- tests/selenium/specs/page_previews.js
+++ tests/selenium/specs/page_previews.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const assert = require( 'assert' ),
-	page = require( '../pageobjects/popups.page' );
+const page = require( '../pageobjects/popups.page' );
 
 describe( 'Dwelling on a valid page link', function () {
 	before( async function () {
@@ -15,24 +14,24 @@ describe( 'Dwelling on a valid page link', function () {
 
 	it( 'I should see a page preview', async function () {
 		await page.dwellPageLink();
-		await assert( await page.seePagePreview(), 'Page preview is shown.' );
+		await expect( await page.pagePopupsSelector ).toBeDisplayed( { message: 'Page preview is shown.' } );
 	} );
 
 	it( 'I should not see a page preview on hash fragment', async function () {
 		await page.dwellPageFragment();
-		assert( await page.doNotSeePagePreview(), 'Page preview is not shown.' );
+		await expect( await page.pagePopupsSelector ).not.toBeDisplayed( { message: 'Page preview is not shown.' } );
 	} );
 
 	it( 'Abandoning link hides page preview', async function () {
 		await page.dwellPageLink();
 		await page.abandonLink();
-		assert( await page.doNotSeePagePreview(), 'Page preview is kept hidden.' );
+		await expect( await page.pagePopupsSelector ).not.toBeDisplayed( { message: 'Page preview is kept hidden.' } );
 	} );
 
 	it( 'Quickly hovering, abandoning and re-hovering a link shows page preview', async function () {
 		await page.hoverPageLink();
 		await page.abandonLink();
 		await page.dwellPageLink();
-		assert( await page.seePagePreview(), 'Page preview is shown.' );
+		await expect( await page.pagePopupsSelector ).toBeDisplayed( { message: 'Page preview is shown.' } );
 	} );
 } );
