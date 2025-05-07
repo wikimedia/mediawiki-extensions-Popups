87e6cc76ce658f9d3f7e661d1d1c6edc0957aa06
gerritbot <ladsgroup+gerritbot@gmail.com>
2025-05-07 10:23:18 +0000

    tests: Migrate bundle size consumers to the new static method
    This is needed because we want to make the provider a static
    method.
    
    Bug: T393122
    Depends-On: If898e0cf90e7478ccd59a2db9bf41eccf5a5e58c
    Change-Id: I5e15cfdfd6a6cd4dc11f4a9672d323d919c43d0f

--- tests/phpunit/structure/BundleSizeTest.php
+++ tests/phpunit/structure/BundleSizeTest.php
@@ -6,7 +6,7 @@ use MediaWiki\Tests\Structure\BundleSizeTestBase;
 class BundleSizeTest extends BundleSizeTestBase {
 
 	/** @inheritDoc */
-	public function getBundleSizeConfig(): string {
+	public static function getBundleSizeConfigData(): string {
 		return dirname( __DIR__, 3 ) . '/bundlesize.config.json';
 	}
 }
