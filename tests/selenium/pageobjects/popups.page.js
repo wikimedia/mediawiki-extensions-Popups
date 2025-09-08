a982f155e8780e702acf8531aba62526158ae7ac
Jon Robson <jdlrobson@gmail.com>
2025-09-08 12:47:28 -0700

    Only make Selenium test pages when they do not exist
    They are needed for every Gerrit patch but not on beta cluster.
    
    Bug: T400694
    Change-Id: I0d75c10e5cadf2d3f1954ff1093b9f3f0310c32d

--- tests/selenium/pageobjects/popups.page.js
+++ tests/selenium/pageobjects/popups.page.js
@@ -10,12 +10,19 @@ const
 	PAGE_POPUPS_LINK_SELECTOR = '.mw-body-content ul a',
 	POPUPS_MODULE_NAME = 'ext.popups.main';
 
+async function pageExists( title ) {
+	const bot = await Api.bot();
+	const response = await bot.read( title );
+	return response.query.pages[ '-1' ] === undefined;
+}
+
 async function makePage( title, path ) {
 	// eslint-disable-next-line security/detect-non-literal-fs-filename
 	const content = fs.readFileSync( path, 'utf-8' );
 	const bot = await Api.bot();
 	await bot.edit( title, content );
 }
+
 class PopupsPage extends Page {
 	get pagePopupsSelector() {
 		return $( '.mwe-popups-type-page' );
@@ -24,8 +31,14 @@ class PopupsPage extends Page {
 	async setupPagePreviews() {
 		return browser.call( async () => {
 			const path = `${ __dirname }/../fixtures/`;
-			await makePage( `${ TEST_PAGE_POPUPS_TITLE } 2`, `${ path }test_page_2.wikitext` );
-			await makePage( TEST_PAGE_POPUPS_TITLE, `${ path }test_page.wikitext` );
+			const exists = await pageExists( TEST_PAGE_POPUPS_TITLE );
+			// Only make the pages if they do not exist.
+			// This allows us to setup the pages in beta cluster differently,
+			// allowing us to workaround issues like T400694.
+			if ( !exists ) {
+				await makePage( `${ TEST_PAGE_POPUPS_TITLE } 2`, `${ path }test_page_2.wikitext` );
+				await makePage( TEST_PAGE_POPUPS_TITLE, `${ path }test_page.wikitext` );
+			}
 		} );
 	}
 
